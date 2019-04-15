307cdb2db18783a9d196864d0be66b5185577623
seaHung <45138439+seaHung@users.noreply.github.com>
2019-04-15 23:10:14 +0800

    springboot actuator执行器

---
+++ springboot-actuator/src/main/java/cn/tellsea/SpringbootActuatorApplication.java
@@ -0,0 +1,17 @@
+package cn.tellsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActuatorApplication.class, args);
+    }
+
+}
+/**
+ * 官方API
+ * https://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-endpoints.html
+ */
