872f89236c754faf6f92c429738d062694a01084
seaHung <45138439+seaHung@users.noreply.github.com>
2019-04-17 19:43:49 +0800

    sms 增加redis端口检测

--- springboot-sms/src/main/java/cn/tellsea/SpringbootSmsApplication.java
+++ springboot-sms/src/main/java/cn/tellsea/SpringbootSmsApplication.java
@@ -1,10 +1,15 @@
 package cn.tellsea;
 
+import cn.tellsea.utils.PortUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootSmsApplication {
+
+    static {
+        PortUtils.checkPort(6379, "Redis 服务端", true);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootSmsApplication.class, args);
---
+++ springboot-sms/src/main/java/cn/tellsea/utils/PortUtils.java
@@ -0,0 +1,41 @@
+/**
+ * 模仿天猫整站 springboot 教程 为 how2j.cn 版权所有
+ * 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
+ * 供购买者学习，请勿私自传播，否则自行承担相关法律责任
+ */
+
+package cn.tellsea.utils;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class PortUtils {
+
+    public static boolean testPort(int port) {
+        try {
+            ServerSocket ss = new ServerSocket(port);
+            ss.close();
+            return false;
+        } catch (java.net.BindException e) {
+            return true;
+        } catch (IOException e) {
+            return true;
+        }
+    }
+
+
+    public static void checkPort(int port, String server, boolean shutdown) {
+        if (!testPort(port)) {
+            if (shutdown) {
+                String message = String.format("在端口 %d 未检查得到 %s 启动%n", port, server);
+                JOptionPane.showMessageDialog(null, message);
+                System.exit(1);
+            } else {
+                String message = String.format("在端口 %d 未检查得到 %s 启动%n,是否继续?", port, server);
+                if (JOptionPane.OK_OPTION != JOptionPane.showConfirmDialog(null, message))
+                    System.exit(1);
+            }
+        }
+    }
+}
