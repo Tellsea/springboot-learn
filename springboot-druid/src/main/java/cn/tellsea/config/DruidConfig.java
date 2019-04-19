bf4bf8c8534826368ba21a866e5a5f65611561c8
seaHung <45138439+seaHung@users.noreply.github.com>
2019-04-19 09:44:00 +0800

    修改IP地址错误问题

--- springboot-druid/src/main/java/cn/tellsea/config/DruidConfig.java
+++ springboot-druid/src/main/java/cn/tellsea/config/DruidConfig.java
@@ -29,7 +29,7 @@ public class DruidConfig {
         log.info("初始化druid servlet配置 ");
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
         // IP白名单
-        servletRegistrationBean.addInitParameter("allow", "*");
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
         // IP黑名单(共同存在时，deny优先于allow)
         servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
         //控制台管理用户
