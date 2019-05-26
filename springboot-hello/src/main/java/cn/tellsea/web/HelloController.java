b77b82f8e813c928c6652702e063bef7bd94f425
seaHung <45138439+seaHung@users.noreply.github.com>
2019-05-26 14:45:20 +0800

    springboot-hello init

--- springboot-hello/src/main/java/cn/tellsea/web/HelloController.java
+++ springboot-hello/src/main/java/cn/tellsea/web/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("")
     @ResponseBody
     public String hello() {
         return "Hello SpringBoot！";
